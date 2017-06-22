d43b907db625bd6d5a4b5f2b261a908b43dc84f8
maxvanbel <maxvanbel@gmail.com>
2017-06-21 22:52:50 -0500

    rework it rework it

---
+++ README.md
@@ -0,0 +1,2 @@
+# ReactStarter
+React/Redux course
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import ArticleList from './components/article_list';
+import ArticleDetail from './components/article_detail';
+const API_KEY = 'AIzaSyBa432OSP8pH4eu0QT8mXj_WBqQH89jp2o';
 
-// Create a new component.  This component should produce some HTML...
-const App = () => {
-  return <div>Hi!</div>
+
+
+
+// create a new component that creates some HTML
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
-
-// Take this component's generated HTML and put it on the page (DOM)
-
-ReactDOM.render(<App />, document.querySelector('.container'))
-
-
-
+// Take this component's generated HTML and show it on the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
