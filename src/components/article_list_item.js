b992608d0a433f9525f3898dfe69875e2bee7ad8
maxvanbel <maxvanbel@gmail.com>
2017-06-29 21:52:03 -0500

    add title and news source to list item

--- src/components/article_list_item.js
+++ src/components/article_list_item.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const ArticleListItem = (props) => {
-  return <li>Article</li>
+  const article = props.article;
+
+  console.log(article.web_url);
+
+  const newsSource = article.source;
+  const headline = article.headline.main;
+  const articleURL = article.web_url; // to be included later
+
+  return (
+    <li className="list-group-item">
+        <div className="article-list media">
+          <div className="media-left">
+            <div className="media-object">{newsSource}</div>
+          </div>
+          <div className="media-body">
+            <div className="media-heading" >{headline}</div>
+          </div>
+        </div>
+    </li>
+  );
 };
 
 export default ArticleListItem;
