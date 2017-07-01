b792d0e7e35e6a8a7993012ed66bee7334cc11d6
maxvanbel <maxvanbel@gmail.com>
2017-07-01 12:51:24 -0500

    add style in style.css

--- src/components/article_list.js
+++ src/components/article_list.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ArticleListItem from './article_list_item';
 
 const ArticleList = (props) => {
+
   const articleItems = props.articles.map((article) => {
     return <ArticleListItem
       onArticleSelect={props.onArticleSelect}
