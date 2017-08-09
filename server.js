5ceed30254dbb846860d9ca30c9a2f206b0e1201
maxvanbel <maxvanbel@gmail.com>
2017-08-08 23:25:31 -0500

    remove public folder

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/index.html'));
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
