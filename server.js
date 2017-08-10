d810eee3557b19a8109a5a3265da0818aa03923c
maxvanbel <maxvanbel@gmail.com>
2017-08-09 20:20:42 -0500

    server fix attempt

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080 ;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 app.listen(port);
 
 require('./controllers/controller.js')(app);
